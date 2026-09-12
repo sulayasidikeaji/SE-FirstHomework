3a8088c134655d98edd7ef250a315b23a94960df
sulayasidikeaji <18299753041@163.com>
2026-09-12 20:00:58 +0800

    add test for count_chars, total 11 test cases

--- test_wordcount.py
+++ test_wordcount.py
@@ -1,9 +1,15 @@
 import unittest
-from wordcount import count_words
+from wordcount import count_words, count_chars
 
 class TestWordCount(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(count_words(""), 0)
+
+    def test_chars_empty(self):
+        self.assertEqual(count_chars(""), 0)
+
+    def test_chars_normal(self):
+        self.assertEqual(count_chars("hello world"), 11)
 
     def test_one_word(self):
         self.assertEqual(count_words("hello"), 1)
@@ -12,10 +18,10 @@ class TestWordCount(unittest.TestCase):
         self.assertEqual(count_words("hello world"), 2)
 
     def test_multi_space(self):
-        self.assertEqual(count_words("hello   world"), 2)
+        self.assertEqual(count_words("hello  world"), 2)
 
     def test_leading_trailing_space(self):
-        self.assertEqual(count_words("   hello world   "), 2)
+        self.assertEqual(count_words("  hello world  "), 2)
 
     def test_punctuation(self):
         self.assertEqual(count_words("hello, world!"), 2)
@@ -28,9 +34,6 @@ class TestWordCount(unittest.TestCase):
 
     def test_mixed_case(self):
         self.assertEqual(count_words("Hello WORLD"), 2)
-
-    def test_many_words(self):
-        self.assertEqual(count_words("a b c d e f"), 6)
 
 if __name__ == '__main__':
     unittest.main()
