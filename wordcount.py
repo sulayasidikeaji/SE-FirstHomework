d4da4b5cff9c26191b872bc7952463800141087e
sulayasidikeaji <18299753041@163.com>
2026-09-12 19:09:59 +0800

    第三次提交：新增字符统计函数，优化输出

--- wordcount.py
+++ wordcount.py
@@ -3,6 +3,10 @@ def count_words(text):
     words = text.split()
     return len(words)
 
+def count_chars(text):
+    return len(text)
+
 if __name__ == "__main__":
     content = "Hello Git and GitHub"
-    print(count_words(content))
+    print("单词数量：", count_words(content))
+    print("字符数量：", count_chars(content))
